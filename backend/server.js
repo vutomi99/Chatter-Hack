efdb912356cd04287bbf6055c2dace0a52c58c65
vutomi99 <vutomi99.ngobeni@gmail.com>
2024-03-09 12:52:13 +0200

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
-const express = require ("express");
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js" 
 
 const app = express();
+dotenv.config();
 
-app.listen(500,()=> console.log('server Running on port 500'));
+const PORT = process.env.PORT || 5000;
+
+
+
+app.get("/",(req,res)=>{
+
+    res.send("Hello world!");
+})
+
+app.use("/api/auth",authRoutes)
+
+
+
+
+app.listen(PORT,()=> console.log(`Server Running on port ${PORT}`));
